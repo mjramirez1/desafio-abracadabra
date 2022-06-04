9397b1f597bd2e16420eda01f94c747a9fca41b0
mjramirez1 <mjramirez1@gmail.com>
2022-06-04 13:42:57 -0400

    Version final funcionando

--- index.js
+++ index.js
@@ -17,22 +17,43 @@ app.listen(3000, () => {
     console.log('El servidor está inicializado en el puerto 3000')
 })
 
-app.use(express.static('assets')) // carpeta assets publica?
+// carpeta assets publica
+app.use(express.static('assets'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + 'index.html')
 })
-
+// recibe usuarios
 app.get('/abracadabra/usuarios', (req, res) => {
     res.send(usuarios)
-}
-)
-
+})
+// Filra por usuario del JSON
 app.use('/abracadabra/juego/:usuario', (req, res, next) => {
-    const { usuario } = req.params 
-    usuario === 
-
-
-    res.sendFile(__dirname + '/usuarios.json')
-}
-)
-
+    const usuario = req.params.usuario
+    const usuariosRegistrados = usuarios.usuarios
+    const findUsuarios = usuariosRegistrados.find((user) => user === usuario)
+    if (findUsuarios) {
+        next()
+    } else {
+        res.redirect('/who.jpeg')
+    }
+})
+app.get('/abracadabra/juego/:usuario', (req, res, next) => {
+    res.redirect('/')
+})
+// Recibe número random
+app.get('/abracadabra/conejo/:n', (req, res) => {
+    const numero = Math.floor(Math.random() * 4) + 1;
+    const n = Number.parseInt(req.params.n)
+    if (n === numero) {
+        res.redirect('/conejito.jpg')
+    } else {
+        res.redirect('/voldemort.jpg')
+    }
+})
+app.get('/voldemort.jpg', (req, res) => {
+    res.redirect('/')
+})
+//ruta generica
+app.get('*', (req, res) => {
+    res.send('<center><h1>Esta página no existe</h1> </center>')
+})
